411fe04825dea632f6955d1a3b4f58f42390f944
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-03-04 14:07:10 -0500

    Update DiscordOAuth2Validators.php

--- src/Validators/DiscordOAuth2Validators.php
+++ src/Validators/DiscordOAuth2Validators.php
@@ -6,28 +6,33 @@ use RPurinton\Exceptions\DiscordOAuth2ValidationException;
 
 class DiscordOAuth2Validators
 {
-    public static function validateClientID(string $client_id): void
+    public static function validateClientID(string|int $client_id): bool
     {
         if (empty($client_id)) throw new DiscordOAuth2ValidationException("Client ID cannot be empty.");
+        return true;
     }
 
-    public static function validateClientSecret(string $client_secret): void
+    public static function validateClientSecret(string $client_secret): bool
     {
         if (empty($client_secret)) throw new DiscordOAuth2ValidationException("Client Secret cannot be empty.");
+        return true;
     }
 
-    public static function validateAuthURI(string $auth_uri): void
+    public static function validateAuthURI(string $auth_uri): bool
     {
         if (empty($auth_uri)) throw new DiscordOAuth2ValidationException("Auth URI cannot be empty.");
+        return true;
     }
 
-    public static function validateTokenURI(string $token_uri): void
+    public static function validateTokenURI(string $token_uri): bool
     {
         if (empty($token_uri)) throw new DiscordOAuth2ValidationException("Token URI cannot be empty.");
+        return true;
     }
 
-    public static function validateInfoURI(string $info_uri): void
+    public static function validateInfoURI(string $info_uri): bool
     {
         if (empty($info_uri)) throw new DiscordOAuth2ValidationException("Info URI cannot be empty.");
+        return true;
     }
 }
